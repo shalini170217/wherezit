f5709b4f5e996b2bdcee3baae4e726bb9f15e9eb
shalini <shalinit.it2023@citchennai.net>
2025-06-06 08:16:54 +0530

    landing page

---
+++ app/_layout.tsx
@@ -0,0 +1,16 @@
+import { Stack } from "expo-router";
+import './global.css';
+ 
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen 
+      name="index"
+      options={{
+        headerShown:false,
+      }}
+
+      />
+    </Stack>
+  );
+}
---
+++ app/index.tsx
@@ -0,0 +1,81 @@
+import { Text, View, Image, TouchableOpacity } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 bg-[#030014]">
+      {/* Top background image */}
+      <Image 
+        source={require('../assets/images/bg.png')}
+        className="absolute w-full h-full z-0"
+        resizeMode="cover"
+      />
+
+      {/* Bottom background image flipped vertically */}
+      <Image
+        source={require('../assets/images/bg.png')}
+        style={{
+          position: "absolute",
+          bottom: 0,
+          width: "100%",
+          height: 400,
+          zIndex: 0,
+          transform: [{ scaleY: -1 }],
+        }}
+        resizeMode="cover"
+      />
+
+      <View
+        className="flex-1 justify-center items-center z-10"
+        style={{ paddingTop: 45 }}
+      >
+        <Text
+          className="text-[#a8a8a7] text-6xl font-extrabold tracking-wider"
+          style={{ fontFamily: "Arial" }}
+        >
+          Wherezit 
+        </Text>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          style={{
+            marginTop: 20,
+            backgroundColor: "#3e4ec7",
+            paddingVertical: 16,
+            paddingHorizontal: 40,
+            borderRadius: 30,
+            shadowColor: "#000",
+            shadowOffset: { width: 0, height: 4 },
+            shadowOpacity: 0.3,
+            shadowRadius: 5,
+            elevation: 8,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text
+            style={{
+              color: "#a8a8a7",
+              fontSize: 20,
+              fontFamily: "Arial",
+              fontWeight: "bold",
+              letterSpacing: 1,
+            }}
+          >
+            Get Started
+          </Text>
+        </TouchableOpacity>
+
+        {/* New image below the button */}
+        <Image
+          source={require('../assets/images/search-137.png')}
+          style={{
+            marginTop: 20,
+            width: 150,
+            height: 150,
+            resizeMode: "contain",
+          }}
+        />
+      </View>
+    </View>
+  );
+}
